051d5d82409fce296425dd4fcd8a868c32602a79
DESKTOP-N4AAHOI\ADMIN <do.quanganh99zz@gmail.com>
2025-10-16 22:17:22 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,9 @@ import { Canvas, NodeDropData } from "@/component/Canvas";
 import { SolutionPanel } from "@/component/SolutionPanel";
 import { Sidebar } from "@/component/Sidebar";
 import { useEffect, useRef, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-
+import "../config/styleOverride.css";
 import styles from "./main.module.scss";
 import { NodeComponent } from "@/entity/NodeComponent";
-import { CONTROL_TYPE, DATA_TYPE, LAYOUT_TYPE } from "@/config/TypeComponent";
-import { XYCoord } from "react-dnd";
 const HomePage = () => {
   const [items, setItems] = useState<NodeComponent[]>([] as NodeComponent[]);
   const [selectedId, setSelectedId] = useState<string | null>(null);
