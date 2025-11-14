51c84e3ce84bf82c6e8ce5c8ea12d02498bbf9e0
doquanganh99zz <do.quanganh99zz@gmail.com>
2025-11-14 21:52:38 +0700

    Thêm style

--- src/config/defineStyle/styles/margin.ts
+++ src/config/defineStyle/styles/margin.ts
@@ -1,6 +1,10 @@
 import { StyleHTML } from "@/entity/canvas/StyleHTML";
 export const margrinKey = "MARGIN";
 export const margrinBottomKey = "MARGIN_BOTTOM";
+export const margrinTopKey = "MARGIN_TOP";
+export const margrinLeftKey = "MARGIN_LEFT";
+export const margrinRightKey = "MARGIN_RIGHT";
+
 export const margrin = {
   key: margrinKey,
   name: "margin",
@@ -28,4 +32,44 @@ export const margrinBottom = {
 } as StyleHTML;
 
 
-export const marginGroupStyle = [margrin, margrinBottom]
+export const margrinTop = {
+  key: margrinTopKey,
+  name: "margin-top",
+  reactObjectName: "marginTop",
+  valueType: "numberPx",
+  validValue: (value: string) => {
+    if (!value) {
+      throw "Giá trị không hợp lệ!";
+    }
+    return true;
+  },
+} as StyleHTML;
+
+
+export const margrinLeft = {
+  key: margrinLeftKey,
+  name: "margin-left",
+  reactObjectName: "marginTop",
+  valueType: "numberPx",
+  validValue: (value: string) => {
+    if (!value) {
+      throw "Giá trị không hợp lệ!";
+    }
+    return true;
+  },
+} as StyleHTML;
+
+
+export const margrinRight = {
+  key: margrinRightKey,
+  name: "margin-right",
+  reactObjectName: "marginRight",
+  valueType: "numberPx",
+  validValue: (value: string) => {
+    if (!value) {
+      throw "Giá trị không hợp lệ!";
+    }
+    return true;
+  },
+} as StyleHTML;
+export const marginGroupStyle = [margrin, margrinBottom, margrinTop, margrinLeft, margrinRight]
--- src/config/defineStyle/styles/width.ts
+++ src/config/defineStyle/styles/width.ts
@@ -1,5 +1,7 @@
 import { StyleHTML } from "@/entity/canvas/StyleHTML";
 export const widthKey = "WIDTH";
+export const minWidthKey = "MIN_WIDTH";
+export const maxWidthKey = "MAX_WIDTH";
 export const width = {
     key: widthKey,
     name: "width",
@@ -12,5 +14,28 @@ export const width = {
         return true;
     },
 } as StyleHTML;
-
-export const widthGroupStyle = [width]
+export const minWidth = {
+    key: minWidthKey,
+    name: "min-width",
+    reactObjectName: "minWidth",
+    valueType: "numberPx",
+    validValue: (value: string) => {
+        if (!value) {
+            throw "Giá trị không hợp lệ!";
+        }
+        return true;
+    },
+} as StyleHTML;
+export const maxWidth = {
+    key: maxWidthKey,
+    name: "max-width",
+    reactObjectName: "maxWidth",
+    valueType: "numberPx",
+    validValue: (value: string) => {
+        if (!value) {
+            throw "Giá trị không hợp lệ!";
+        }
+        return true;
+    },
+} as StyleHTML;
+export const widthGroupStyle = [width, minWidth, maxWidth]
