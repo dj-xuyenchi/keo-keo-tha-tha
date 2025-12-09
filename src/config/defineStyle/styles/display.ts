1bc152e9095d88902297fee3f1e2daa1f0552d29
DESKTOP-N4AAHOI\ADMIN <do.quanganh99zz@gmail.com>
2025-12-09 23:31:27 +0700

    [anhdq]
    update flex

--- src/config/defineStyle/styles/display.ts
+++ src/config/defineStyle/styles/display.ts
@@ -1,9 +1,20 @@
 import { StyleHTML } from "@/entity/canvas/StyleHTML";
 export const DISPLAY_KEY = "DISPLAY_KEY";
+export const ALIGN_ITEMS_KEY = "ALIGN_ITEMS_KEY";
+export const JUSTIFY_CONTENT_KEY = "JUSTIFY_CONTENT_KEY";
 export const display = {
   key: DISPLAY_KEY,
   name: "display",
   reactObjectName: "display",
 } as StyleHTML;
-
-export const displayGroupStyle = [display];
+export const alignItems = {
+  key: ALIGN_ITEMS_KEY,
+  name: "align-items",
+  reactObjectName: "alignItems",
+} as StyleHTML;
+export const justifyContent = {
+  key: JUSTIFY_CONTENT_KEY,
+  name: "justify-content",
+  reactObjectName: "justifyContent",
+} as StyleHTML;
+export const displayGroupStyle = [display, alignItems, justifyContent];
